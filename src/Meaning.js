194b6161123e355592f7f4a40e8c7b6305507aa3
Liandermans <139888264+Liandermans@users.noreply.github.com>
2024-07-13 16:21:32 +0200

    Added if-statement for examples

--- src/Meaning.js
+++ src/Meaning.js
@@ -5,12 +5,19 @@ export default function Meaning(props) {
   let partOfSpeech = props.meaning.partOfSpeech;
   let definition = props.meaning.definition;
   let example = props.meaning.example;
+  if (example === null) {
+    example = "-";
+  }
 
   return (
     <div className="Meaning">
       <h5>{partOfSpeech}</h5>
-      <p>{definition}</p>
       <p>
+        <strong>Definition: </strong>
+        {definition}
+      </p>
+      <p>
+        <strong>Example: </strong>
         <em>{example}</em>
       </p>
     </div>
