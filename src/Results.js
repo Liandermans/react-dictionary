3fade0014cf48f4ff7202c3743efe2f199d28309
Liandermans <139888264+Liandermans@users.noreply.github.com>
2024-07-19 15:55:29 +0200

    Adapted display of phonetics

--- src/Results.js
+++ src/Results.js
@@ -2,9 +2,6 @@ import React from "react";
 import Meaning from "./Meaning";
 
 export default function Results(props) {
-  console.log(props.results);
-  // response.data.meanings[0];
-
   if (props.results) {
     let phonetic = props.results.phonetic;
     let word = props.results.word;
@@ -12,7 +9,7 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{word}</h2>
-        <h4>{phonetic}</h4>
+        <h4 className="phonetic">[{phonetic}]</h4>
         {props.results.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
