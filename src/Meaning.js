07926c80ac17ce87e18b494de7e1b15e39f613de
Liandermans <139888264+Liandermans@users.noreply.github.com>
2024-07-19 13:28:46 +0200

    Integrated synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,25 +1,52 @@
 import React from "react";
 
 export default function Meaning(props) {
-  console.log(props.meaning);
   let partOfSpeech = props.meaning.partOfSpeech;
   let definition = props.meaning.definition;
   let example = props.meaning.example;
+  let synonyms = props.meaning.synonyms;
+
   if (example === null) {
     example = "-";
   }
 
-  return (
-    <div className="Meaning">
-      <h5>{partOfSpeech}</h5>
-      <p>
-        <strong>Definition: </strong>
-        {definition}
-      </p>
-      <p>
-        <strong>Example: </strong>
-        <em>{example}</em>
-      </p>
-    </div>
-  );
+  if (synonyms === null) {
+    return (
+      <div className="Meaning">
+        <h5>{partOfSpeech}</h5>
+        <p>
+          <strong>Definition: </strong>
+          {definition}
+        </p>
+        <p>
+          <strong>Example: </strong>
+          <em>{example}</em>
+        </p>
+        <p>
+          <strong>Synonyms: </strong>-
+        </p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="Meaning">
+        <h5>{partOfSpeech}</h5>
+        <p>
+          <strong>Definition: </strong>
+          {definition}
+        </p>
+        <p>
+          <strong>Example: </strong>
+          <em>{example}</em>
+        </p>
+        {props.meaning.synonyms.map(function (synonym, index) {
+          return (
+            <ul className="synonyms">
+              <li key={index}>{synonym}</li>
+            </ul>
+          );
+        })}
+      </div>
+    );
+  }
 }
